545295e43e68acc7dc9f5137b680f70b932e6f34
youssefM <youssef.ait.malek1@gmail.com>
2024-12-26 23:59:55 +0100

    Finished grid section

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,7 +1,16 @@
-/* eslint-disable @next/next/no-img-element */
 /* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @next/next/no-img-element */
+ 'use client'
 import { cn } from "@/lib/utils";
 import { BackgroundGradientAnimation } from "./GradientBg";
+import { animate, useSpring } from "framer-motion";
+import { useState } from "react";
+import Lottie from "react-lottie";
+import animationData from "@/data/confetti.json";
+import { MagicButton } from "./MagicButton";
+import { IoCopyOutline } from "react-icons/io5";
+
+ 
 
 export const BentoGrid = ({
   className,
@@ -42,10 +51,15 @@ export const BentoGridItem = ({
   spareImg?: string;
 
 }) => {
+   const [copied, setCopied] = useState(false);
+   const handleCopy=()=>{
+        navigator.clipboard.writeText('contact@malekYoussef.com');
+        setCopied(true);
+   }
   return (
     <div
       className={cn(
-        "row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4 ",
+        "row-span-1 relative overflow-hidden rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4 border border-white/[0.1]",
         className
       )}
 
@@ -55,7 +69,7 @@ export const BentoGridItem = ({
           "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
       }}
     >
-      <div className={`${id === 6} && 'flex justify-center h-full'`}>
+      <div className={`${id === 6 && 'flex justify-center'} h-full'`}>
         <div className="w-full h-full absolute">
           {img && (
             <img 
@@ -90,10 +104,62 @@ export const BentoGridItem = ({
           {title}
         </div>
        
-       </div>
      
+  
+       {id === 3 &&(
+
+        <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
+
+          <div className="flex flex-col gap-3 lag:gap-8">
+
+            {['React.js','Next.js','TypeScript'].map((item)=> (
+
+              <span key={item} className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">
+                {item}
+              </span>
+            ))}
+               <span className="py-4 px-3 rounded-lg text-center bg-[#10123e]"/>
+
+          </div>
+         
+          <div className="flex flex-col gap-3 lag:gap-8">
+          <span className="py-4 px-3 rounded-lg text-center bg-[#10123e]"/>
+            {['Vue.js','AWS','MongoDB'].map((item)=> (
+
+              <span key={item} className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">
+                {item}
+              </span>
+            ))}
+               
+
+          </div>
+        </div> 
+       )}
+          { id === 6 && (
+              <div className="mt-5 relative">
+
+                <div className={`absolute -bottom-5 right-0`}>
+                  <Lottie options={{
+                    loop: copied,
+                    autoplay: copied,
+                    animationData,
+                    rendererSettings:{
+                      preserveAspectRatio : 'xMidYMid slice',
+                    }
+                  }} />
+                </div>
+
+                <MagicButton title={copied ? 'Email copied' : 'Copy my Email'} icon={<IoCopyOutline />} position="left" 
+                otherClasses="!bg-[161a31]"
+                handleClick={handleCopy}
+                />
+              </div>
+             
+          )}
+       
       </div>
       
+    </div>
     </div>
   );
 };
--- components/ui/MagicButton.tsx
+++ components/ui/MagicButton.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from "react";
+import React from "react";
 
 export const MagicButton = ({
   title,
@@ -17,6 +17,7 @@ export const MagicButton = ({
     <button
       className="relative inline-flex h-12 w-full overflow-hidden rounded-lg
      p-[1px] focus:outline-none md:w-60 md:mt-10 "
+     onClick={handleClick}
     >
       <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
       <span
